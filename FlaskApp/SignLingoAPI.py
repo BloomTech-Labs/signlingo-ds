5241f5c5b1520e37f10edb034b99a57d9f66a861
Wesley Mountford <goe_horus@hotmail.com>
2020-05-20 17:11:04 -0500

    API can now accept video, saves it into a temporary file, splits it into temporary images, and cleans up once the api call finishes.

--- FlaskApp/SignLingoAPI.py
+++ FlaskApp/SignLingoAPI.py
@@ -1,12 +1,18 @@
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, Response
 from werkzeug.utils import secure_filename
 from flask import jsonify
-import os
+import os, time
 
 from HelperFunctions import splitter, clear_temp, allowed_file
+from detector import main as img_detector
 
 
 app = Flask(__name__, template_folder='templates')
+if not os.path.isdir('TEMPPICS'):
+    os.mkdir('TEMPPICS')
+if not os.path.isdir('TEMPVID'):
+    os.mkdir('TEMPVID')
+
 
 @app.route('/')
 def home():
@@ -14,6 +20,7 @@ def home():
 
 @app.route('/api', methods=['POST'])
 def api():
+    start_time = time.time()
     if request.method == 'POST':
         # Checks to make sure a file was actually received with the key 'video'
         if 'video' not in request.files:
@@ -33,7 +40,7 @@ def api():
             print('Video saved.\n')
 
         for vid in os.listdir('TEMPVID'):
-            splitter(vid, frameskip=10) #Frameskip allows us to designate that we only save frames with a count % frameskip. 1 saves every frame.
+            splitter(vid, frameskip=30) #Frameskip allows us to designate that we only save frames with a count % frameskip. 1 saves every frame.
 
         print("Files in temporary folder:")
         print("Temp Video folder contents:", os.listdir('TEMPVID'))
@@ -43,12 +50,17 @@ def api():
         predict_list = [('a', 0.443), ('a', 0.852), ('s',0.241), ('m', 0.159)] #Clear this out before actually making predictions. Data here is for testing.
         # for img in os.listdir('TEMPPICS'):
         #     predict_list.append(model.predict(img)) #Or however we want to predict the result.
-
+        classes, confidences = img_detector()
+        print("TYPES - ", type(classes), type(confidences))
+        x = list(zip(classes, confidences))
+        print(x)
         clear_temp() # Helper function that clears both of the temporary folders.
 
         print("Temp folders cleared.")
         print("Temp Video folder contents:", os.listdir('TEMPVID'))
         print("Number of files in TEMPPICS:", len(os.listdir('TEMPPICS')), '\n')
 
-
-        return jsonify(predict_list) # Or some variant thereof
+        end_time = time.time()
+        print("TOTAL RUN TIME = ", (end_time - start_time))
+        return str(x)
+        #return jsonify(predict_list) # Or some variant thereof
---
+++ FlaskApp/detector.py
@@ -0,0 +1,162 @@
+import numpy as np
+import argparse, time, cv2, os
+
+CONF_THRES=0.01 # Confidence Threshold for detection reporting.
+NMS_THRES=0.1 # Non-Maxima Suppression threshold, shouldn't need to be changed.
+YOLO_PATH="./"
+
+def get_labels(labels_path):
+    """
+    Loads the labels used by our YOLO model
+    """
+    lpath = os.path.sep.join([YOLO_PATH, labels_path])
+    LABELS = open(lpath).read().strip().split("\n")
+    return LABELS
+
+def get_colors(LABELS):
+    """
+    Initializes a list of colors to represent each possible class label.
+    """
+    np.random.seed(1337)
+    COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+    return COLORS
+
+def get_weights(weights_path):
+    """
+    Loads the weights used by the YOLO model.
+    """
+    weightsPath = os.path.sep.join([YOLO_PATH, weights_path])
+    return weightsPath
+
+def get_config(config_path):
+    """
+    Gets the config used by the YOLO model.
+    """
+    configPath = os.path.sep.join([YOLO_PATH, config_path])
+    return configPath
+
+def load_model(config_path, weights_path):
+    """
+    Loads the model
+    """
+    print("[INFO] loading YOLO...")
+    net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
+    return net
+
+def get_prediction(image, net, LABELS, COLORS):
+    """
+    Gets prediction from the image
+    """
+
+    #Getting image height/width
+    (img_height, img_width) = image.shape[:2]
+
+    #Determining output layer names
+    output_layer_names = net.getLayerNames()
+    output_layer_names = [output_layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    #Constructs a blob from the image input
+    blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
+                                 swapRB=True, crop=False)
+
+    net.setInput(blob)
+
+    start = time.time()
+
+    layer_outputs = net.forward(output_layer_names)
+
+    #print("Layer Outputs:\n", layer_outputs)
+
+    end = time.time()
+
+    #Show timing info
+    print("[INFO] YOLO took {:.6f} seconds.".format(end - start))
+
+    #Initializing our list of bounding boxes, confidences, and class ids.
+    b_boxes = []
+    confidences = []
+    class_ids = []
+
+    #loop over each of the layer outputs.
+    for output in layer_outputs:
+        # Loop over each detection
+        for detection in output:
+            #Extracting class id and confidence
+            scores = detection[5:]
+            #print(scores)
+
+            class_id = np.argmax(scores)
+
+            confidence = scores[class_id]
+
+            if confidence > CONF_THRES:
+                #If our confidence is greater than our confidence threshold, we'll draw some bounding boxes.
+
+                #YOLO actually returns the coordinates of the center of the object it detected, and the box's width/height.
+                #We'll scale the box's dimensions depending on the original image dimensions.
+                box = detection[0:4] * np.array([img_width, img_height, img_width, img_height])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                #Use the center coordinates to derive the top and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                #Update our lists of bounding box coordinates, confidences, and class IDs
+                b_boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                class_ids.append(class_id)
+
+    # Applying non-maxima suppression to suppress weak, overlapping bounding boxes.
+    idxs = cv2.dnn.NMSBoxes(b_boxes, confidences, CONF_THRES, NMS_THRES)
+
+    # Ensuring at least one detection exists
+    if len(idxs) > 0:
+        #Loop over the indexes we are keeping.
+        for i in idxs.flatten():
+            #Extracting bounding box coordinates.
+            (x, y) = (b_boxes[i][0], b_boxes[i][1])
+            (w, h) = (b_boxes[i][2], b_boxes[i][3])
+
+            #draw a bounding box rectangle and label
+            color = [int(c) for c in COLORS[class_ids[i]]]
+            cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+
+            text = "{}: {:.4f}".format(LABELS[class_ids[i]], confidences[i])
+
+            cv2.putText(image, text, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    return image, class_ids, confidences
+
+def main():
+    labels_path = os.path.join('detector_dependencies','names.list')
+    cfg_path = os.path.join('detector_dependencies', 'yolov3_custom_test.cfg')
+    weights_path = os.path.join('detector_dependencies', 'yolov3_custom_4000.weights')
+
+    labels = get_labels(labels_path)
+    config = get_config(cfg_path)
+    weights = get_weights(weights_path)
+
+    nets = load_model(config, weights)
+
+    colors = get_colors(labels)
+    classes = []
+    confids = []
+    for img in os.listdir('TEMPPICS'):
+        image = cv2.imread(os.path.join('TEMPPICS', img))
+
+        result_img, class_ids, confidences = get_prediction(image, nets, labels, colors)
+        classes.append(class_ids)
+        confids.append(confidences)
+
+        print("Predicted class ids:", class_ids)
+        print("Predicted confidence levels", confidences)
+
+        cv2.imshow("Image", result_img)
+        cv2.waitKey()
+
+    return classes, confids
+
+
+
+if __name__ == '__main__':
+    main()
