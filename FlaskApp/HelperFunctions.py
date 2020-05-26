ef739e83668cdd105476347a86454674131be8f5
Wesley Mountford <goe_horus@hotmail.com>
2020-05-26 12:02:59 -0500

    Added config

--- FlaskApp/HelperFunctions.py
+++ FlaskApp/HelperFunctions.py
@@ -26,31 +26,31 @@ import numpy as np
 
 ALLOWED_EXTENSIONS = {'mp4'}
 
-def splitter(video, frameskip=1):
-    """
-    Accepts a video file as first param.
-    frameskip must be an integer, only frames with a count % frameskip == 0 will be saved.
-    frameskip == 1 keeps all frames. frameskip == 2 drops every other frame, etc.
-    Outputs selected frames into TEMPPICS folder.
-    """
-    print('splitter reading', video)
-
-    cap = cv2.VideoCapture(os.path.join('TEMPVID',video))
-
-    count = 0
-    frame_list = []
-    while (cap.isOpened()):
-        ret, frame = cap.read()
-
-        if ret:
-            if count % frameskip == 0:
-                #print('Read %d frame: ' % count, ret)
-                cv2.imwrite(os.path.join('TEMPPICS', 'frame{:d}.jpg'.format(count)), frame)
-            count += 1
-        else:
-            break
-
-    return frame_list
+# def splitter(video, frameskip=1):
+#     """
+#     Accepts a video file as first param.
+#     frameskip must be an integer, only frames with a count % frameskip == 0 will be saved.
+#     frameskip == 1 keeps all frames. frameskip == 2 drops every other frame, etc.
+#     Outputs selected frames into TEMPPICS folder.
+#     """
+#     print('splitter reading', video)
+#
+#     cap = cv2.VideoCapture(os.path.join('TEMPVID',video))
+#
+#     count = 0
+#     frame_list = []
+#     while (cap.isOpened()):
+#         ret, frame = cap.read()
+#
+#         if ret:
+#             if count % frameskip == 0:
+#                 #print('Read %d frame: ' % count, ret)
+#                 cv2.imwrite(os.path.join('TEMPPICS', 'frame{:d}.jpg'.format(count)), frame)
+#             count += 1
+#         else:
+#             break
+#
+#     return frame_list
 
 def allowed_file(filename):
     """
