93eb350b6900df53e54e8cfbc6e8a7df3d3d19f2
unknown <coopervos1@gmail.com>
2020-05-27 12:08:59 -0500

    unit test page

---
+++ FlaskApp/unit_tests.py
@@ -0,0 +1,27 @@
+import unittest
+import random
+
+class TestStringMethods(unittest.TestCase):
+
+    # def test_upper(self):
+    #     self.assertEqual('foo'.upper(), 'FOO')
+
+    # def test_isupper(self):
+    #     self.assertTrue('FOO'.isupper())
+    #     self.assertFalse('Foo'.isupper())
+
+    # def test_split(self):
+    #     s = 'hello world'
+    #     self.assertEqual(s.split(), ['hello', 'world'])
+    #     # check that s.split fails when the separator is not a string
+    #     with self.assertRaises(TypeError):
+    #         s.split(2)
+
+    def test_api(self):
+        random_bit = random.getrandbits(1)
+        random_boolean = bool(random_bit)
+        
+        self.assertTrue(type(random_boolean) == bool)
+
+if __name__ == '__main__':
+    unittest.main()
